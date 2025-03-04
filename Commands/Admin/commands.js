776b0f2682b12d6bb96250064ec40905a102d0c5
J33-1 <97525305+J33-1@users.noreply.github.com>
2025-03-04 19:21:32 +0530

    Update commands.js

--- Commands/Admin/commands.js
+++ Commands/Admin/commands.js
@@ -1,1 +1,19 @@
+const admins = ["8113007514@s.whatsapp.net"];
 
+function handleAdminCommand(command, sender) {
+    if (!admins.includes(sender)) {
+        return "Sorry Buddy, it ain'tgonna work.";
+    }
+
+    if (command === "/run") {
+        return "Clem is up and running now!";
+    } else if (command === "/info") {
+        return "V 2.0  ///  Developed by #71392"
+    } else if (command === "/end") {
+        return "Shutting down... (Not really, but I got the command!)";
+    } else {
+        return "Don't get it.";
+    }
+}
+
+module.exports = handleAdminCommand;
