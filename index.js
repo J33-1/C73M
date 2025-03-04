9d85ca24f522cddc2f4878ec5db54d1a25c9db30
J33-1 <97525305+J33-1@users.noreply.github.com>
2025-03-04 17:49:37 +0530

    Update index.js

--- index.js
+++ index.js
@@ -1,5 +1,1 @@
-Clem-Bot/
-│── bot.js              # Main data file - Clem
-│── commands.js         # Clem- commands & responses
-│── plugins.js          # Plugin manager
-└── package.json        # Dependencies manager
+
