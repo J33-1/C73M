126a84aaba9505bea073f6370840d9732afa992e
J33-1 <97525305+J33-1@users.noreply.github.com>
2025-03-04 19:25:20 +0530

    Update index.js

--- index.js
+++ index.js
@@ -1,1 +1,22 @@
+const C73M = require("C73M");
+const handleAdminCommand = require("./C73M/Commands/Admin/commands");
+const handleUserCommand = require("./C73M/Commands/User/user_commands");
 
+C73M.create().then(client => start(client));
+
+function start(client) {
+    client.onMessage(async message => {
+        let body = message.body.trim();
+        if (!body.startsWith("/")) return;
+        let command = body.slice(1).toLowerCase();
+        let sender = message.sender.id;
+        if (command === "/run" || command === "/end") {
+            let response = handleAdminCommand(command, sender);
+            client.sendText(message.from, response);
+        } 
+        else {
+            let response = handleUserCommand(command);
+            client.sendText(message.from, response);
+        }
+    });
+}
